54595d851d01129b258493cfe1d917c6b50e7d46
Nadeem <anwarnadeem2010@gmail.com>
2021-06-28 17:06:21 +0530

    delete event

--- unplash-project/src/components/SearchBar.js
+++ unplash-project/src/components/SearchBar.js
@@ -4,9 +4,6 @@ class SearchBar extends React.Component {
     onInputChange(event){
     console.log(event.target.value)
     }
-    onInputClick(){
-        console.log("input clicked")
-    }
 
 
 
@@ -16,7 +13,7 @@ class SearchBar extends React.Component {
                 <form className="ui form">
                     <div className="field">
                         <label>Image Search </label>
-                        <input type = 'text' onClick={this.onInputClick} onChange={this.onInputChange}/>
+                        <input type = 'text'  onChange={this.onInputChange}/>
                     </div>
                 </form>
             </div>
